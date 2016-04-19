7b74bd32e1674d22a42683d92eed80f1a3af09ad
azu <azuciao@gmail.com>
2016-04-19 13:14:20 +0900

    chore(storegroup): add fallback

--- src/js/framework/UILayer/StoreGroup.js
+++ src/js/framework/UILayer/StoreGroup.js
@@ -76,7 +76,8 @@ export default class StoreGroup extends CoreEventEmitter {
              */
             const prevState = this._storeValueMap.get(store.name);
             const nextState = store.getState(prevState);
-            const nextStateName = nextState.name || nextState.constructor.name;
+            // State.name || Store.name
+            const nextStateName = nextState.constructor.name || store.name;
             assert(nextStateName, `${store.name}.getState() should return Object that should have .name property.`);
             if (prevState && this._previousChangingStores.indexOf(store) === -1) {
                 return {
